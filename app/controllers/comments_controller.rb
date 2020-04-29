e71a40bda22a863d7088fc03e0fdd011a76f7fae
Chris Jones <cjones92@my.westga.edu>
2020-04-28 23:16:41 -0400

    Enabled comments to be made on the website

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -69,6 +69,6 @@ class CommentsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def comment_params
-      params.require(:comment).permit(:comment)
+      params.require(:comment).permit(:comment, :restaurant_id, :user_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
   get '/upvote/:id', to: 'restaurants#vote_for_restaurant', as: 'upvote'
   get '/downvote/:id', to: 'restaurants#vote_against_restaurant', as: 'downvote'
   get '/favorite_restaurant/:id', to: 'restaurants#favorite_a_restaurant', as: 'pick'
+  get '/comment_restaurant/:id', to: 'comments#new', as: 'commenting'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
